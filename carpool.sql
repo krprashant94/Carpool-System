e39fc45fb1805e15b7e717b67660c8047a95e43e
Prasahnt Kumar <kr.prashant94@gmail.com>
2019-06-27 12:06:48 +0530

    Update carpool.sql

--- carpool.sql
+++ carpool.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 22, 2019 at 10:09 AM
+-- Generation Time: Jun 27, 2019 at 08:36 AM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -27,20 +27,28 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `applicatioin` (
-  `id` varchar(20) NOT NULL,
   `draft_id` varchar(20) NOT NULL,
   `applicant` int(11) NOT NULL,
   `receiver` int(11) NOT NULL,
   `message` text NOT NULL,
   `application_date` varchar(10) NOT NULL,
   `department` varchar(50) NOT NULL,
-  `req_date` varchar(10) NOT NULL,
-  `ending_date` varchar(10) NOT NULL,
+  `req_date` varchar(15) NOT NULL,
+  `ending_date` varchar(15) NOT NULL,
   `vehicle_req` varchar(50) NOT NULL,
-  `vechile_issue` varchar(20) NOT NULL,
+  `vehicle_issue` varchar(20) NOT NULL,
   `notification` varchar(1) NOT NULL,
+  `applied` varchar(1) NOT NULL DEFAULT 'N',
   `issued_by` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `applicatioin`
+--
+
+INSERT INTO `applicatioin` (`draft_id`, `applicant`, `receiver`, `message`, `application_date`, `department`, `req_date`, `ending_date`, `vehicle_req`, `vehicle_issue`, `notification`, `applied`, `issued_by`) VALUES
+('OBAL2342', 6, 0, '', '', '', '', '', '', '', 'Y', 'N', ''),
+('TDJI1911', 6, 1, 'hg', '', '1', '2019-06-13', '2019-06-11', 'car', '', 'Y', 'Y', '');
 
 -- --------------------------------------------------------
 
@@ -49,7 +57,7 @@ CREATE TABLE `applicatioin` (
 --
 
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+  `id` bigint(20) NOT NULL,
   `password` varchar(64) NOT NULL,
   `image` text NOT NULL,
   `f_name` varchar(20) NOT NULL,
@@ -57,33 +65,50 @@ CREATE TABLE `user` (
   `surname` varchar(20) NOT NULL,
   `dob` varchar(10) NOT NULL,
   `house_no` varchar(10) NOT NULL,
-  `address l1` varchar(20) NOT NULL,
-  `address l2` varchar(20) NOT NULL,
+  `address_l1` varchar(20) NOT NULL,
+  `address_l2` varchar(20) NOT NULL,
   `landmark` varchar(50) NOT NULL,
   `pincode` int(6) NOT NULL,
+  `city` varchar(20) NOT NULL,
+  `state` varchar(20) NOT NULL,
   `country` varchar(20) NOT NULL,
-  `mail_id` varchar(25) NOT NULL,
-  `phone` int(10) NOT NULL,
-  `blood_group` varchar(2) NOT NULL,
+  `mail_id` varchar(50) NOT NULL,
+  `phone` int(12) NOT NULL,
+  `blood_group` varchar(3) NOT NULL,
   `identification` text NOT NULL,
   `department` varchar(50) NOT NULL,
   `auth_level` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `password`, `image`, `f_name`, `m_name`, `surname`, `dob`, `house_no`, `address_l1`, `address_l2`, `landmark`, `pincode`, `city`, `state`, `country`, `mail_id`, `phone`, `blood_group`, `identification`, `department`, `auth_level`) VALUES
+(2, '40bd001563085fc35165329ea1ff5c5ecbdbbeef', '', '', '', '', '', '', '', '', '', 0, '', '', '', 'pamelabanerjee11@gmail.com', 0, '', '', '', 0),
+(6, '5723fd42d69df94be995fe69de96dbbe0b12d1d5', '', 'Prashant', 'Kumar', 'Prasad', '', '', '', '', '', 832108, '', '', '', 'pkp@pkp.com', 2147483647, 'A+', '', 'Accounts', 0);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `vechile`
+-- Table structure for table `vehicle`
 --
 
-CREATE TABLE `vechile` (
-  `v_no` varchar(20) NOT NULL,
-  `v_type` varchar(20) NOT NULL,
+CREATE TABLE `vehicle` (
+  `no` varchar(20) NOT NULL,
+  `type` varchar(20) NOT NULL,
   `subtype` varchar(20) NOT NULL,
   `status` varchar(10) NOT NULL,
   `location` varchar(30) NOT NULL,
   `app_id` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `vehicle`
+--
+
+INSERT INTO `vehicle` (`no`, `type`, `subtype`, `status`, `location`, `app_id`) VALUES
+('', '', '', '', '', '');
 
 --
 -- Indexes for dumped tables
@@ -93,21 +118,21 @@ CREATE TABLE `vechile` (
 -- Indexes for table `applicatioin`
 --
 ALTER TABLE `applicatioin`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `draft_id` (`draft_id`);
+  ADD PRIMARY KEY (`draft_id`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `mail_id` (`mail_id`);
+  ADD UNIQUE KEY `mail_id` (`mail_id`),
+  ADD UNIQUE KEY `phone` (`phone`);
 
 --
--- Indexes for table `vechile`
+-- Indexes for table `vehicle`
 --
-ALTER TABLE `vechile`
-  ADD PRIMARY KEY (`v_no`);
+ALTER TABLE `vehicle`
+  ADD PRIMARY KEY (`no`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -117,7 +142,7 @@ ALTER TABLE `vechile`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
