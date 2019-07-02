705be7c4c0147b8ff6d04a7c73c19974ba5f81a0
Prasahnt Kumar <kr.prashant94@gmail.com>
2019-07-02 15:52:08 +0530

    Update read_only_view.php

--- read_only_view.php
+++ read_only_view.php
@@ -10,10 +10,16 @@
 	
 	$application_details = $a->fetch_by_id('draft_id', $_GET['view_id'])[0];
 	$applicant_name = $u->getName($application_details['applicant'])[0];
-	$applicant_details = $u->fetch_by_id('id', $application_details['applicant']);
+	$applicant_details = $u->fetch_by_id('id', $application_details['applicant'])[0];
 
-	print_r($application_details);
-	print_r($applicant);
+	if ($application_details['forwarded'] != 0) {
+		$reciver = $application_details['forwarded'];
+	}else{
+		$reciver = $application_details['receiver'];
+	}
+
+	$reciver_name = $u->getName($reciver)[0];
+	$reciver_details = $u->fetch_by_id('id', $reciver)[0];
  ?>
 
 <!DOCTYPE html>
@@ -36,20 +42,21 @@
 	<div class="box">
 
 		To,<br>
-		[to_name]<br>
-		[department]<br><br>
+		<?=$reciver_name['name']?><br>
+		<?=$reciver_name['department']?><br><br>
 		
-		[date]<br><br>
+		Date : <?=$application_details['application_date']?><br><br>
 		<b><i>
-			Subject :Requesting for a vichle from [date] to [date]<br>
-			Pickup Location : [location]<br><br>
+			Subject :Requesting for a vichle from <?=$application_details['start_date']?> to <?=$application_details['ending_date']?><br>
+			Pickup Location : <?=$application_details['pickup_location']?><br><br>
 		</i></b>
-		Sir,
-		[msg]<br><br>
+		Sir,<br>
+		<?=$application_details['message']?><br><br>
 
-		<?=$applicant['name']?><br>
+		<?=$applicant_name['name']?><br>
 		<?=$applicant_details['department']?><br>
-		<a href="tel:123">+91 <?=$applicant_details['phone']?></a><br>
+		<a href="tel:<?=$applicant_details['phone']?>">+91 <?=$applicant_details['phone']?></a><br>
+		<a href="mailto:<?=$applicant_details['mail_id']?>"><?=$applicant_details['mail_id']?></a><br>
 
 		<br><br>
 
