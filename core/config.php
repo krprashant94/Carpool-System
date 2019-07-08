f15834bcc92f1c1bd0ebcee4e1eb1049674dbde5
Prasahnt Kumar <kr.prashant94@gmail.com>
2019-07-08 12:46:00 +0530

    Delete config.php

--- core/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-		$email = "kr.prashant94@gmail.com";
-		$user_pass = "12345";
-		$host = "localhost";
-		$path = "/tata";
-		$db_name = "carpool";
-		$db_user = "root";
-		$db_pass = "";
-?>
