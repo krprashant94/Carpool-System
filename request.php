09076fcf84af117d01b48e6e184314adbe89d03c
Prasahnt Kumar <kr.prashant94@gmail.com>
2019-07-08 13:36:44 +0530

    code cleanup
    Co-Authored-By: Pamela Banerjee <pamelabanerjee11@users.noreply.github.com>

--- request.php
+++ request.php
@@ -37,9 +37,9 @@
 				<table class="table table-striped table-hover" id="list_table">
 					<thead class="thead-dark">
 						<tr>
-							<th scope="col" style="width: 10%;">Draft no</th>
+							<th scope="col">Draft no</th>
 							<th scope="col">Details</th>
-							<th scope="col" style="width: 20%;">Operation</th>
+							<th scope="col">Operation</th>
 						</tr>
 					</thead>
 					<tbody class="applicatio_list">
--- userlist.php
+++ userlist.php
@@ -5,7 +5,7 @@
 	}
 	include 'core/application.db.php';
 	$a = new Application($host, $db_name, $db_user, $db_pass);
-	$draft_list = $a->applicant_joint_details($_SESSION['user_id'], 'N');
+	$draft_list = $a->applicant_joint_details($_SESSION['user_id']);
 ?>
 <!DOCTYPE html>
 <html>
@@ -16,12 +16,6 @@
 		include 'core/meta.php';
 	?>
 	<link rel="stylesheet" href="css/core.css">
-	<style type="text/css">
-		#list_table img{
-			cursor: pointer;
-		}
-	</style>
-
 </head>
 <body>
 	<?php
@@ -31,44 +25,34 @@
 		<div class="row">
 			<div class="col col-lg-3 side_nav">
 				<?php
-					$active = "draft";
+					$active = "history";
 					require "core/side_nav.php";
 				?>
 			</div>
 			<div class="col-md-9">
+
 				<nav class="navbar navbar-light bg-light">
-					<a class="navbar-brand">Draft List</a>
+					<a class="navbar-brand">History</a>
 				</nav>
 				<table class="table table-striped table-hover" id="list_table">
 					<thead class="thead-dark">
 						<tr>
-							<th scope="col">Draft no</th>
-							<th scope="col">Details</th>
+							<th scope="col">Serial no</th>
+							<th scope="col">User</th>
+							<th scope="col">Status</th>
 							<th scope="col">Operation</th>
 						</tr>
 					</thead>
-					<tbody>
-
-						<?php foreach ($draft_list as $key => $value): ?>
-						<tr class="<?=$value['draft_id'];?>">
-							<th scope="row"><?=$value['draft_id'];?></th>
-							<td>
-								You applied for <i><?php if($value['vehicle_req']) echo $value['vehicle_req']; else echo "vehicle";?></i> <small><?=date('d-m-Y, h:i A', $value['application_date']);?></small><br>
-								For <i><?=date('d-m-Y, h:i A', $value['start_date']);?> to <?=date('d-m-Y, h:i A', $value['ending_date']);?></i>
-							</td>
-							<td>
-								<center>
-									<a href="application.php?draft_id=<?=$value['draft_id'];?>"><img onclick="edit('<?=$value['draft_id'];?>')" src="images/edit.png" title="Edit" width="20px"></a> | 
-									<img onclick="delete_application('<?=$value['draft_id'];?>')" src="images/delete.png" title="Delete this draft" width="20px">
-								</center>
-							</td>
+					<tbody  class="applicatio_list">
+						<tr>
+							<td>1</td>
+							<td></td>
+							<td></td>
+							<td></td>
 						</tr>
-						<?php endforeach ?>
-
 
 					</tbody>
 				</table>
-
 
 
 
@@ -76,17 +60,17 @@
 		</div>
 	</div>
 
-</body>
+<?php  include_once 'core/model_operation.inc.php'; ?>
 
+</body>
 <script type="text/javascript" src="js/jquery.js"></script>
 <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/swal.js"></script>
 
+<script type="text/javascript" src="js/listoperation.js"></script>
 
 <link rel="stylesheet" type="text/css" href="DataTables/datatables.min.css"/>
 <script type="text/javascript" src="DataTables/datatables.min.js"></script>
-<script type="text/javascript" src="js/listoperation.js"></script>
-
 
 <script type="text/javascript">
 	$(document).ready( function () {
