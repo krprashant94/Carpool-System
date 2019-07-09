30376ebfb44aa2179495e015b72ca9759224a3e3
Prasahnt Kumar <kr.prashant94@gmail.com>
2019-07-09 13:31:51 +0530

    Update meta.php
    meta cooected

--- core/meta.php
+++ core/meta.php
@@ -5,12 +5,12 @@
 	<meta name="language" content="en">
 	<meta name="robots" content="index, follow">
 
-	<meta name="og:url" property="og:url" content="<?php echo $_SERVER['HTTP_HOST']; ?>"/>
+	<meta name="og:url" property="og:url" content="<?php echo $_SERVER['REQUEST_SCHEME'].'://'.$host.$path; ?>"/>
 	<meta property="og:site_name" content="Car pool network">
 
 	<link rel="shortcut icon" href="favicon.ico" />
 
-	<meta name="og:image" property="og:image" content="[poster-url]">
+	<meta name="og:image" property="og:image" content="<?php echo $_SERVER['REQUEST_SCHEME'].'://'.$host.$path; ?>/images/tata_composit_logo.png">
 	<meta property="og:image:type" content="image/jpg">
 	<meta property="og:image:width" content="128">
 	<meta property="og:image:height" content="128">
