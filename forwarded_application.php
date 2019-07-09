427b5c37ad2da96e6005f387976b8c55fbe9e0dc
Prasahnt Kumar <kr.prashant94@gmail.com>
2019-07-09 15:15:26 +0530

    Update forwarded_application.php

--- forwarded_application.php
+++ forwarded_application.php
@@ -46,9 +46,9 @@
 				<table class="table table-striped table-hover" id="list_table">
 					<thead class="thead-dark">
 						<tr>
-							<th scope="col" style="width: 10%;">Draft ID</th>
-							<th scope="col">setails</th>
-							<th scope="col" style="width: 20%;">Operation</th>
+							<th scope="col">Draft ID</th>
+							<th scope="col">Details</th>
+							<th scope="col">Operation</th>
 						</tr>
 					</thead>
 					<tbody class="applicatio_list">
